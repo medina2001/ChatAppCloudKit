5506b4308a747727782449553f356af6ba60f179
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2020-11-25 16:04:26 -0400

    Tela de Cadastro finalizada. Cadastro acontecendo, salvando no Banco de Dados.

---
+++ ChatProject/Controller/ChatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewController.swift
+//  ChatProject
+//
+//  Created by Gabriel de Oliveira Maciel on 25/11/20.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+    }
+
+}
--- ChatProject/Controller/ViewController.swift
+++ ChatProject/Controller/ViewController.swift
@@ -5,15 +5,81 @@
 //  Created by Gabriel de Oliveira Maciel on 24/11/20.
 //
 
+import CloudKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var UserNameText: UITextField!
+    @IBOutlet weak var CadastrarButton: UIButton!
+    
+    @IBAction func Cadastrar(_ sender: Any) {
+        if UserNameText.text == ""{
+            let alert = UIAlertController(title: "Atenção", message: "Preencha o campo obrigatório acima com o seu nome!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (_) in}))
+            self.present(alert,animated: true, completion: nil)
+        }else{
+            performSegue(withIdentifier: "cadastroFinalizado", sender: self)
+            let Username = UserNameText.text ?? ""
+            cadastrarUsuário(Username: Username)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let nextVC = segue.destination as! ChatViewController
+        }
+    
+    let privateDatabase = CKContainer(identifier: "iCloud.ChatApp").privateCloudDatabase
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func cadastrarUsuário(Username: String){
+        let predicate = NSPredicate(format: "name == %@", Username)
+        
+        let query = CKQuery(recordType: "ChatUser", predicate: predicate)
+        
+        let operation = CKQueryOperation(query: query)
+        
+        var existed = false
+        operation.recordFetchedBlock = { record in
+            DispatchQueue.main.async {
+                existed = true
+            }
+        }
+        
+        operation.queryCompletionBlock = { cursor, error in
+            DispatchQueue.main.async {
+                if existed{
+                    print("Nome de usuário já existe!")
+                    let alert = UIAlertController(title: "Atenção", message: "Já existe um usuário com este nome na nuvem. Tente Novamente.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (_) in}))
+                    self.present(alert,animated: true, completion: nil)
+                }else{
+                    print("Salvou no Banco de Dados!")
+                    let record = CKRecord(recordType: "ChatUser")
+                    record.setValue(Username, forKey: "name")
+                    self.privateDatabase.save(record) { (savedRecord, error) in
+                        DispatchQueue.main.async{
+                            if error == nil{
+                                let alert = UIAlertController(title: "Ótimo! ;)", message: "O novo usuário, \(Username), foi criado com sucesso!", preferredStyle: .alert)
+                                
+                                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                                self.present(alert,animated: true, completion: nil)
+                            }else{
+                                let alert = UIAlertController(title: "Eita", message: "Deu erro em alguma coisa...\n" + error!.localizedDescription, preferredStyle: .alert)
+                                
+                                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                                self.present(alert,animated: true, completion: nil)
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        privateDatabase.add(operation)
+    }
 }
-
